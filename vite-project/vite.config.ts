baeca9827600f21e9966e2d7e5f2119a401c5f19
haeussma <83341109+haeussma@users.noreply.github.com>
2024-12-30 11:26:24 +0100

    working tauri

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
 import path from "path"
-
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
@@ -9,5 +9,17 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+
+  // Add these lines:
+  clearScreen: false,
+  server: {
+    strictPort: true,
+  },
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
   },
 })

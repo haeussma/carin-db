c5619ab334b4daf8cb8fe7174df2cc10933697dc
haeussma <83341109+haeussma@users.noreply.github.com>
2025-05-19 19:25:10 +0200

    removed unused

--- frontend/app/visualize/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function VisualizePage() {
-    // Change the URL below to your desired destination
-    redirect("/browser/");
-    return null;
-}
